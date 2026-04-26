fbbe3fb613f81ee55f37e00dc7828bdab16ce905
joshwaroshan36-dev <joshwaroshan36@gmail.com>
2026-04-26 15:26:06 +0530

    Time: 44 ms (28.60%) | Memory: 49.5 MB (67.85%) - LeetSync

---
+++ 721-accounts-merge/accounts-merge.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        Map<String, String> parent = new HashMap<>();
+        Map<String, String> owner = new HashMap<>();
+
+        for (List<String> acc : accounts) {
+            String name = acc.get(0);
+            for (int i = 1; i < acc.size(); i++) {
+                parent.put(acc.get(i), acc.get(i));
+                owner.put(acc.get(i), name);
+            }
+        }
+
+        for (List<String> acc : accounts) {
+            String p = find(acc.get(1), parent);
+            for (int i = 2; i < acc.size(); i++) {
+                parent.put(find(acc.get(i), parent), p);
+            }
+        }
+
+        Map<String, TreeSet<String>> map = new HashMap<>();
+
+        for (String email : parent.keySet()) {
+            String root = find(email, parent);
+            map.computeIfAbsent(root, k -> new TreeSet<>()).add(email);
+        }
+
+        List<List<String>> res = new ArrayList<>();
+        for (String root : map.keySet()) {
+            List<String> list = new ArrayList<>();
+            list.add(owner.get(root));
+            list.addAll(map.get(root));
+            res.add(list);
+        }
+
+        return res;
+    }
+
+    private String find(String x, Map<String, String> parent) {
+        if (!parent.get(x).equals(x)) {
+            parent.put(x, find(parent.get(x), parent));
+        }
+        return parent.get(x);
+    }
+}
